07aaa1d12ce9b6b668cf396ce5e7434f37c7b7cb
James Ainooson <jainooson@gmail.com>
2017-05-23 17:17:30 -0500

    Multiple modals can now be displayed

--- build/fzui.js
+++ build/fzui.js
@@ -29,34 +29,37 @@ fzui.dropdowns = new dropdown();
 $(function() {
     fzui.dropdowns.init();
 })
-;/**
+;fzui.modalCount = 0;
+
+/**
  * Create a modal window.
  * @params selector A CSS selector used for selecting the contents of the modal
  * @params options An object or string that contains options for the model window creator.
  */
 fzui.modal = function(selector, options){
-    if($('body').hasClass('modal-active')) return;
     var backdrop = $('<div></div>');
     backdrop.addClass('modal-backdrop');
+    backdrop.attr('id', 'modal-backdrop-' + fzui.modalCount);
     
     var modal = $('<div></div>');
 
     var close = $('<button></button>');
     var content = $(selector).clone(); 
-    var width = content.width() == 0 ? 700 : content.width();
+    var width = $(selector).outerWidth(true);
+
     $(selector).remove();
     close.addClass('close-button');
     modal.append(close);
     modal.addClass('modal-wrapper');
+    modal.attr('modal-wrapper-' + fzui.modalCount);
     modal.width(width);
     modal.css({left: $(window).width()/2 - width/2 });
     modal.append(content);
     content.removeClass("modal");
-    content.addClass('current-modal');
+    //content.addClass('current-modal');
 
     backdrop.append(modal);
     $('body').append(backdrop);
-    $('body').addClass('modal-active');
 
     backdrop.fadeIn('fast', function(){
         modal.css('opacity', '0.0');
@@ -67,21 +70,18 @@ fzui.modal = function(selector, options){
         }, 'fast');
     })
 
-    close.on('click.fzui', fzui.closeModal);
-};
-
-fzui.closeModal = function(){
-    $('.modal-wrapper').animate({
-            top:"-20",
-            opacity: 0
-        }, 'fast', 
-        function(){
-        $('body').append($('.current-modal'));
-        $('.current-modal').addClass('modal').removeClass('current-modal');
-        $('.modal-backdrop').fadeOut('fast', function(){
-            $('.modal-wrapper').remove();
-            $('.modal-backdrop').remove();
-            $('body').removeClass('modal-active');
+    close.on('click.fzui', function(){
+        $(modal).animate({
+                top:"-20",
+                opacity: 0
+            }, 'fast', 
+            function(){
+            $('body').append(content);
+            content.addClass('modal');//.removeClass('current-modal');
+            backdrop.fadeOut('fast', function(){
+                modal.remove();
+                backdrop.remove();
+            });
         });
     });
 };
