70431d516581f4ead804ca2f2e46d496e5eb676e
James Ainooson <jainooson@gmail.com>
2017-12-29 21:50:57 -0600

    Added a dropup

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,7 +3,7 @@
  */
 function dropdown() {
     var resetContents = function (event) {
-        $('.dropdown').each(function(){
+        $('.dropdown, .dropup').each(function(){
             // Reset all dropdowns on body click
             if(event.type == 'click' && event.target.parentNode === $(this)[0]) return;
             $(this).removeClass('active');
@@ -11,23 +11,27 @@ function dropdown() {
     }
 
     this.init = function(container){
-        container.find('.dropdown > .dropdown-right').each(function(i, item){
+        container.find('.dropdown > .dropdown-right, .dropup > .dropup-right').each(function(i, item){
             var button = $(item).prev();
             $(item).css({left: button.offset().left + button.outerWidth() - $(item).outerWidth()});
-            console.log();
         });
-        container.find('.dropdown > button, .dropdown > .button, .dropdown > .clickable').click(function(event){
+        var query = '.dropdown > button, .dropdown > .button, .dropdown > .clickable,' +
+                    '.dropup > button, .dropup > .button, .dropup > .clickable';
+        container.find(query).click(function(event) {
             resetContents(event);
-            var parent;
-            parent = $(this).parent();
+            var button = $(this);
+            var parent = button.parent();
+            var content = button.next();
             parent.toggleClass('active');
-            if($(this).next().hasClass('dropdown-right')) {
-                $(this).next().css({left: $(this).offset().left + $(this).outerWidth() - $(this).next().outerWidth()});
+            if(content.hasClass('dropdown-right') || content.hasClass('dropup-right')) {
+                content.css({left: button.offset().left + button.outerWidth() - content.outerWidth()});
+            }
+            if(parent.hasClass('dropup')) {
+                content.css({top:button.offset().top - content.outerHeight(true)});
             }
             event.stopPropagation();
         });
     }
-
     $(document).on('click.fzui', resetContents);
 }
 
